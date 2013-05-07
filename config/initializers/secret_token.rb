b374ad3576f443034b585f8caa01f5dfe79738c3
MichelleMHey <michellemhey@gmail.com>
2013-05-07 09:54:16 -0500

    removed

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RubyCloset::Application.config.secret_token = '085c6b5303f0105d9f3b006e10c445c93a74a8f1201e0428f685f44c6c48851921829388db6f3bc55c5d1741a01ed01dc25d7a175c4228d93438d82b9ac4c944'
