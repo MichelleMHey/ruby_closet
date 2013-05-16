01e28fe6859efa8e214e7f9fb49971d0041c8b0b
MichelleMHey <michellemhey@gmail.com>
2013-05-15 21:42:22 -0500

    Complete initial test by adding migrations

--- test/test_adding_project.rb
+++ test/test_adding_project.rb
@@ -2,11 +2,12 @@ require 'test_helper'
 
 class TestAddingProject < Test::Unit::TestCase
   def test_takes_arguments_and_saves_them
-    # # start with no projects
-    # assert_equal Project.count, 0
-    # # `ruby futureperfect add foo`
-    # Project.create( name: 'foo' )
-    # # end up with a 'foo' project
-    # assert_equal Project.count, 1
+    # start with no projects
+    assert_equal Project.count, 0
+    # `ruby futureperfect add foo`
+    project = Project.create( name: 'foo' )
+    # project => #<Project:12312 @name='foo'>
+    # end up with a 'foo' project
+    assert_equal Project.count, 1
   end
 end
