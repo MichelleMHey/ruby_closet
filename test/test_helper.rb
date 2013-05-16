acfc1cc3250dc793f210600cc8fe7e474da372f4
MichelleMHey <michellemhey@gmail.com>
2013-05-16 10:54:21 -0500

    Added remove a project

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,8 @@ require 'test/unit'
 require_relative '../bootstrap_ar'
 
 module DatabaseCleaner
-	def after_teardown
-		super
-		Project.destroy_all
-	end
+  def before_setup
+    super
+    Project.destroy_all
+  end
 end
