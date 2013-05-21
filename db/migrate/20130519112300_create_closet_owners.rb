234da29edb8bf014af6be431e803f491e3e825de
MichelleMHey <michellemhey@gmail.com>
2013-05-21 11:27:08 -0500

    removed

--- db/migrate/20130519112300_create_closet_owners.rb
+++
@@ -1,8 +0,0 @@
-class CreateClosetOwners < ActiveRecord::Migration
-  def change
-    create_table :closet_owners do |t|
-      t.string :name
-      t.integer :closet_id
-    end
-  end
-end
--- db/migrate/20130519122300_create_closets.rb
+++
@@ -1,7 +0,0 @@
-class CreateClosets < ActiveRecord::Migration
-  def change
-    create_table :closets do |t|
-      t.integer :closet_owner_id
-    end
-  end
-end
