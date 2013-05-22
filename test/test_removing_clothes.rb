f416ae0a143e2aef4c522629ea858af0527ee5ae
MichelleMHey <michellemhey@gmail.com>
2013-05-22 10:35:07 -0500

    edit tests to pass deleted white space

--- test/test_removing_clothes.rb
+++ test/test_removing_clothes.rb
@@ -3,9 +3,28 @@ require 'test_helper'
 class TestRemovingClothes < MiniTest::Unit::TestCase
   include DatabaseCleaner
   
-  def test_07_remove_only_clothing
-    Clothing.create( name: 'Dress')
-    `ruby testcloset remove "Dress"`
+  def test_remove_only_one_clothing_name
+    Clothing.create( name: 'only child')
+    `ruby testcloset remove "only child"`
     assert Clothing.all.empty?
+  end
+
+  def test_remove_particular_piece_of_clothing
+    Clothing.create( name: 'a' )
+    Clothing.create( name: 'b' )
+    Clothing.create( name: 'c' )
+    assert !Clothing.where( name: 'b').all.empty?
+    `ruby testcloset remove b`
+    assert Clothing.where( name: 'b').all.empty?
+  end
+
+  def test_remove_particular_clothing_doesnt_remove_all
+    assert Clothing.all.empty?
+    Clothing.create( name: 'a' )
+    Clothing.create( name: 'b' )
+    Clothing.create( name: 'c' )
+    assert !Clothing.where( name: 'b' ).all.empty?
+    `ruby testcloset remove b`
+    assert_equal 2, Clothing.count
   end
 end
