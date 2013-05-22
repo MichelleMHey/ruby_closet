b8379474e91700413f915d75ba491b0f806a17d6
MichelleMHey <michellemhey@gmail.com>
2013-05-22 10:28:13 -0500

    edit tests to pass deleted white space

--- test/test_listing_multiple_clothes.rb
+++ test/test_listing_multiple_clothes.rb
@@ -20,23 +20,3 @@ EOS
     assert_equal expected, actual
   end
 end
-
-
-
-#   def test_listing_when_there_are_no_projects
-#     assert Clothing.all.empty?
-#     actual = `ruby testcloset list`
-#     assert_equal "", actual
-#   end
-
-#   def test_listing_multiply_projects
-#     Clothing.create(name: 'Dress')
-#     Clothing.create(name: 'Pants')
-#     actual = `ruby testcloset list`
-#     expected = <<EOS
-# 1. Dress
-# 2. Pants
-# EOS
-#     assert_equal expected, actual
-#   end
-# end
