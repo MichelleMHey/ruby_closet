e792a7bcaa3b84420d2c8c449878ef0de8400a94
MichelleMHey <michellemhey@gmail.com>
2013-05-22 10:20:42 -0500

    Fixed last test error message

--- test/test_adding_clothes.rb
+++ test/test_adding_clothes.rb
@@ -3,27 +3,27 @@ require 'test_helper'
 class TestAddingClothes < MiniTest::Unit::TestCase
   include DatabaseCleaner
 
-  def test_01_adding_clothes_and_saves_them
+  def test_take_clothing_names_and_saves_it
     assert_equal 0, Clothing.count
-    `ruby testcloset add Dress`
-    assert_equal 1, Clothing.count
+    `ruby testcloset add Jeans`
+    assert_equal 1, Clothing.count  
   end
 
-  def test_02_takes_clothes_and_uses_them
-    `ruby testcloset add Dress`
-    assert_equal 'Dress', Clothing.last
+  def test_take_clothing_names_and_uses_them
+    `ruby testcloset add Jeans`
+    assert_equal 'Jeans', Clothing.last.name
   end
 
-  def test_03_duplicate_names_are_ignored
-    Clothing.create( name: 'Dress' )
-    original_clothes_count = Clothing.count
-    `ruby testcloset add Dress`
-    assert_equal original_clothes_count, Clothing.count
+  def test_duplicate_clothing_names_are_ignored
+    Clothing.create( name: 'Jeans' )
+    original_clothing_count = Clothing.count
+    `ruby testcloset add Jeans`
+    assert_equal original_clothing_count, Clothing.count
   end
- 
-  def test_04_duplicate_names_outputs_error_message
-    Clothing.create( name: 'Dress' )
-    results = `ruby testcloset add Dress`
-    assert results.include?("Name already exists!"), "Actually was '#{results}'"
+
+  def test_duplicate_clothing_names_outputs_error_message
+    Clothing.create( name: 'Jeans' )
+    results = `ruby testcloset add Jeans`
+    assert results.include?('Clothing already exists!'), "Actually was '#{results}'"
   end
 end
