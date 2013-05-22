ccddc30458636a2d904666690bd1c4082fc0ec2e
MichelleMHey <michellemhey@gmail.com>
2013-05-22 12:06:57 -0500

    edit table

--- db/migrate/20130521101400_create_clothes.rb
+++ db/migrate/20130521101400_create_clothes.rb
@@ -1,9 +1,11 @@
 class CreateClothes < ActiveRecord::Migration
   def change
     create_table :clothings do |t|
-      t.string :name #description
-      t.integer :style_id
-      t.integer :temperature_id
+      t.string :name # description
+      t.string :style
+      t.string :temperature # hot, cold, warm
+      # t.integer :style_id
+      # t.integer :temperature_id
     end
   end
 end
